0bd13a155a18ec90ebda4efaa51b0c3dd45275cc
hyphenminus <36642485+hyphenminus@users.noreply.github.com>
2022-05-09 18:24:56 +0100

    format types of post, parse self markdown / html

--- src/features/main/Main.js
+++ src/features/main/Main.js
@@ -11,6 +11,7 @@
 // import styles from './Main.module.css';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+import IframeResizer from 'iframe-resizer-react';
 import defaultReddit from '../../store/default.json';
 
 // TODO: https://www.npmjs.com/package/snudown-js
@@ -19,7 +20,7 @@ export function Main() {
   // console.dir(defaultReddit.data);
   return (
     <div>
-      <ul>
+      <ul style={{overflowX: 'hidden'}}>
         {defaultReddit.data.children.map(item => <Article key={item.data.name} data={item.data} />)}
       </ul>
             {/* {JSON.stringify(defaultReddit)} */}
@@ -34,7 +35,7 @@ export function Article(props) {
           <h3 className="title">{props.data.title}</h3>
           <small>[{props.data.post_hint}]</small>
           {/* <h4>PROPS: ((((({JSON.stringify(props)})))))</h4> */}
-          <div className="selftext"><ReactMarkdown children={props.data.selftext} remarkPlugins={[remarkGfm]} /></div>
+          {props.data.selftext_html ? <div>Selftext: <Selftext selftext_markdown={props.data.selftext} selftext_html={props.data.selftext_html} title={props.data.name} /></div> : false}
           {props.data.url ? <div>Url: <a href={props.data.url}>{props.data.url}</a></div> : false}
           {props.data.thumbnail ? <div>Thumbnail: <img src={props.data.thumbnail} alt={props.data.title}  /></div> : false}
           {props.data.post_hint === 'image' ? <div>Image: <img src={props.data.url} alt={props.data.title} width={'25%'} /></div> : false}
@@ -48,9 +49,29 @@ export function Article(props) {
 
 export function Property(props) {
   return (
-        <div key={props.data.name}>
-            {Object.entries(props.data).map(entry => <span key={entry[0]}><b>{entry[0]}:</b> <span>{entry[0] === 'all_awardings' ? '...' : JSON.stringify(entry[1])}</span>; </span> )}
-        </div>
+    <div key={props.data.name}>
+      {Object.entries(props.data).map(entry => <span key={entry[0]}><b>{entry[0]}:</b> <span>{entry[0] === 'all_awardings' ? '...' : JSON.stringify(entry[1])}</span>; </span> )}
+    </div>
+);
+}
+
+export function Selftext(props) {
+  const selftext_html = '<!doctype html><body>' + props.selftext_html.replaceAll('&lt;', '<').replaceAll('&gt;', '>').replaceAll('&amp;', '&') + '</body>';
+  return (
+    <div>
+      <div className="selftext" style={{border: 'solid green'}}>
+        <ReactMarkdown children={props.selftext_markdown} remarkPlugins={[remarkGfm]} />
+      </div>
+      <div className="selftext" style={{border: 'solid blue'}}>
+        {/* <iframe srcdoc={'<!doctype html><body>' + props.selftext_html  + '</body>'} title={props.title} height='100%' width='100%'></iframe> */}
+        <IframeResizer
+          log
+          srcDoc={selftext_html} 
+          title={props.title}
+          style={{ width: '1px', minWidth: '100%'}}
+        />
+      </div>
+    </div>
   );
 }
 
@@ -63,4 +84,3 @@ export function Video(props) {
   
     );
 }
-
